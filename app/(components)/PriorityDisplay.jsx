114c20e6a6fede9a37bacb8da6232c37d8df4c92
unknown <thomasjjohnson9311@gmail.com>
2024-08-08 19:53:38 -0500

    created progressdisplay component

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const PriorityDisplay = () => {
   return (
-    <div>
+    <div className="flex justify-start align-baseline">
       <FontAwesomeIcon icon={faFire} className="text-red-400" />
       <FontAwesomeIcon icon={faFire} className="text-red-400" />
       <FontAwesomeIcon icon={faFire} className="text-red-400" />
