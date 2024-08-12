8b17d9af5d4ad1b31212739487e38890dc8e955c
unknown <thomasjjohnson9311@gmail.com>
2024-08-11 19:16:37 -0500

    dynamically update progress bar

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -18,11 +18,11 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">08/8/24 8:05PM</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>
